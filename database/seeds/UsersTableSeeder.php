b030cb76c6fae17ebd164478d9a4db13b0a262d1
青木友里 <aokiapa081001@gmail.com>
2024-02-23 23:59:43 +0900

    初期データの登録実装

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,6 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        //初期ユーザー
+        DB::table('users')->insert([
+            [
+                'over_name' => 'Divus',
+                'under_name' => 'Crewel',
+                'over_name_kana' => 'デイヴィス',
+                'under_name_kana' => 'クルーウェル',
+                'mail_address' => 'Crewel@gmail.com',
+                'sex' => '1',
+                'birth_day' => '19610125',
+                'role' => '3',
+                'password' => bcrypt('Crewel')
+            ]
+        ]);
     }
 }
